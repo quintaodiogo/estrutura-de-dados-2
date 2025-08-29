dbe3950a55509834de649daa5bd03f04df557fb9
Diogo Quintão <quintaodiogo@gmail.com>
2025-08-28 22:19:06 -0300

    refatorar implementação do Shellsort e corrigir impressão do vetor

--- lab3.c
+++ lab3.c
@@ -1,18 +1,43 @@
 /*Diogo de Paula Quintão(202207108) e João Pedro da Costa Reis(202206124)*/
+
+/*Implementação do Método de Ordenação Shellsort, seguindo a sequência de Knuth (h=(h*3)+1).
+Imprimir o vetor para cada sequência de h-ordenado.*/
 #include <stdio.h>
-#define TAMANHO 6
+#define TAMANHO 5
 #include <locale.h>
 
-void Funcao(int A[], int n)
+void shellSort(int A[], int n)
 {
-    
+    int i, j, h, temp;
+    h = 1;
+    while (h < n)
+    {
+        h = (h * 3) + 1;
+    }
+    while (h > 0)
+    {
+        h /= 3;
+        for (i = h; i < n; i++)
+        {
+            temp = A[i];
+            j = i - h;
+            while (j >= 0 && temp < A[j])
+            {
+                A[j + h] = A[j];
+                j -= h;
+            }
+            A[j + h] = temp;
+        }
+        printf("Vetor h-ordenado para h = %d: ", A);
+        imprimirVetor(A, n);
+    }
 }
 
 void imprimirVetor(int v[], int n)
 {
     // Função para imprimir os elementos do vetor
     int i;
-    for (i = 1; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         printf("%d ", v[i]);
     }
@@ -23,18 +48,12 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese");
     // Declaração de variáveis
-    int i, v1[TAMANHO] = {0, 8, 4, 1, 7, 3};
+    int i, v1[TAMANHO] = {8, 4, 1, 7, 3};
 
     // Exibição dos vetores originais
     printf("O vetor original é: \n");
     imprimirVetor(v1, TAMANHO);
 
     // Ordenação dos vetores
-    Funcao(v1, TAMANHO);
-
-    // Exibição dos resultados
-    printf("\nO vetor após a ordenação é: \n");
-    imprimirVetor(v1, TAMANHO);
-
-    printf("\nO maior valor do vetor é: %d\n", v1[TAMANHO]);
-} 
+    shellSort(v1, TAMANHO);
+}
