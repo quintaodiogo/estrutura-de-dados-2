b5a162657f6f3926cc72134f1527828d8df44752
Diogo <quintaodiogo@gmail.com>
2025-10-18 21:50:49 -0300

    fix: remove espaços

--- Lab7/Código/tabela_hash.c
+++ Lab7/Código/tabela_hash.c
@@ -21,33 +21,25 @@ TabelaHash* criar_tabela(int M) {
         perror("Falha ao alocar TabelaHash");
         exit(1);
     }
-
     nova_tabela->M = M;
     nova_tabela->colisoes = 0;
-
     nova_tabela->baldes = (Lista**) malloc(M * sizeof(Lista*));
     if (nova_tabela->baldes == NULL) {
         perror("Falha ao alocar vetor de baldes");
         free(nova_tabela);
         exit(1);
     }
-
     for (int i = 0; i < M; i++) {
         nova_tabela->baldes[i] = criar_lista();
     }
-
     return nova_tabela;
 }
 
 void inserir_na_tabela(TabelaHash* tabela, const char* chave) {
     if (tabela == NULL) return;
-
     unsigned long valor_int = string_to_int(chave);
-
     int indice = funcao_hash(valor_int, tabela->M);
-
     int colidiu = inserir_na_lista(tabela->baldes[indice], chave);
-
     if (colidiu) {
         tabela->colisoes++;
     }
@@ -55,19 +47,15 @@ void inserir_na_tabela(TabelaHash* tabela, const char* chave) {
 
 void liberar_tabela(TabelaHash* tabela) {
     if (tabela == NULL) return;
-
     for (int i = 0; i < tabela->M; i++) {
         liberar_lista(tabela->baldes[i]);
     }
-
     free(tabela->baldes);
-
     free(tabela);
 }
 
 void imprimir_tabela(TabelaHash* tabela) {
     if (tabela == NULL) return;
-
     printf("--- TABELA HASH (M=%d) ---\n", tabela->M);
     for (int i = 0; i < tabela->M; i++) {
         printf("[%03d]: ", i);
